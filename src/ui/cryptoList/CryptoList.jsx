c51a79799fddf6f3a3d6f6433713e2740b3a0f88
shanetoken <pphu@business.fb.com>
2024-04-08 18:05:44 +0800

    finish list data without up and down style

--- src/ui/cryptoList/CryptoList.jsx
+++ src/ui/cryptoList/CryptoList.jsx
@@ -22,23 +22,34 @@ export default function CryptoList() {
     <div>
       {coinData.length > 0 ? (
         <ul>
-            <li className="list_header font-semibold flex w-full justify-between py-2 px-5 border-[1px] border-gray-100">
-                <div>#</div>
-                <div className="w-40">Name</div>
-                <div>Price</div>
-                <div>1h %</div>
-                <div>24h %</div>
-                <div>7d %</div>
-            </li>
+          <li className="list_header font-semibold flex w-full justify-between py-2 px-5 border-[1px] border-gray-100">
+            <div>#</div>
+            <div className="w-40">Name</div>
+            <div>Price</div>
+            <div>1h %</div>
+            <div>24h %</div>
+            <div>7d %</div>
+          </li>
 
           {coinData.map((coin) => (
-            <li key={coin.id} className="list_header font-semibold flex w-full justify-between py-2 px-5 border-[1px] border-gray-100">
+            <li
+              key={coin.id}
+              className="list_header font-semibold flex w-full justify-between items-center py-2 px-5 border-[1px] border-gray-100"
+            >
               <div>{coin.market_cap_rank}</div>
-                <div className="w-40"><span>{coin.image}</span>{coin.name}</div>
-                <div>{coin.current_price}</div>
-                <div>{coin.price_change_percentage_24h}</div>
-                <div>{coin.market_cap_change_percentage_24h}</div>
-                <div>{coin.ath_change_percentage}</div>
+              <div className="w-40 flex items-center space-x-2">
+                <span>
+                  <img src={coin.image} alt="coinImage" className="w-6" />
+                </span>
+                <div className="flex flex-col justify-center">
+                  <div>{coin.name}</div>
+                  <div className="text-grap-400 uppercase">{coin.symbol}</div>
+                </div>
+              </div>
+              <div>{coin.current_price}</div>
+              <div>{coin.price_change_percentage_24h}</div>
+              <div>{coin.market_cap_change_percentage_24h}</div>
+              <div>{coin.ath_change_percentage}</div>
             </li>
           ))}
         </ul>
